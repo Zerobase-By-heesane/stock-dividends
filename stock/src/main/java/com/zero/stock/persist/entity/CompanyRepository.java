86d7cbd8643b463128cdc3bfae060fc0aea600e9
heesane <tees3359@gmail.com>
2024-06-27 02:54:34 +0900

    Company Repository

---
+++ stock/src/main/java/com/zero/stock/persist/entity/CompanyRepository.java
@@ -0,0 +1,8 @@
+package com.zero.stock.persist.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity,Long> {
+}
