f8e63050157dad51867a9c0687902df936e2e334
heesane <tees3359@gmail.com>
2024-06-27 03:03:19 +0900

    ScrapedResult 객체

---
+++ stock/src/main/java/com/zero/stock/model/ScrapedResult.java
@@ -0,0 +1,19 @@
+package com.zero.stock.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ScrapedResult {
+    private Company company;
+    private List<Dividend> dividendEntities;
+
+    public ScrapedResult(){
+        this.dividendEntities = new ArrayList<>();
+    }
+
+}
