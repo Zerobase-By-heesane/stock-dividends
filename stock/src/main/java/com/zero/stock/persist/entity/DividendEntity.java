2739ede1cec3f10685857d9ddf48e87c863d581a
heesane <tees3359@gmail.com>
2024-06-27 02:54:24 +0900

    Dividend Entity

---
+++ stock/src/main/java/com/zero/stock/persist/entity/DividendEntity.java
@@ -0,0 +1,29 @@
+package com.zero.stock.persist.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity(name = "dividend")
+@Getter
+@ToString
+@NoArgsConstructor
+public class DividendEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long companyId;
+
+    private LocalDateTime date;
+
+    private String dividend;
+
+}
