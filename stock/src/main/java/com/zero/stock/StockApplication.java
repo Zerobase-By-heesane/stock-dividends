9d5adec27d7426b60aa72d300d701f0d16c9ab17
heesane <tees3359@gmail.com>
2024-06-27 02:39:00 +0900

    필요없는 코드 삭제

--- stock/src/main/java/com/zero/stock/StockApplication.java
+++ stock/src/main/java/com/zero/stock/StockApplication.java
@@ -1,13 +1,19 @@
 package com.zero.stock;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class StockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockApplication.class, args);
-	}
-
+        SpringApplication.run(StockApplication.class, args);
+    }
 }
