4aa0e2c4688f08f8582c26b2d32f012ca98264b8
heesane <tees3359@gmail.com>
2024-06-27 23:59:44 +0900

    ScrapperResult

--- stock/src/main/java/com/zero/stock/model/ScrapedResult.java
+++ stock/src/main/java/com/zero/stock/model/ScrapedResult.java
@@ -10,10 +10,10 @@ import java.util.List;
 @AllArgsConstructor
 public class ScrapedResult {
     private Company company;
-    private List<Dividend> dividendEntities;
+    private List<Dividend> dividends;
 
     public ScrapedResult(){
-        this.dividendEntities = new ArrayList<>();
+        this.dividends = new ArrayList<>();
     }
 
 }
