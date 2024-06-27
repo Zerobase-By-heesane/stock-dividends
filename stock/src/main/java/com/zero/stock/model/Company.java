2e3c0c83004e5f942c1268c0349f81ce1f83853b
heesane <tees3359@gmail.com>
2024-06-28 04:14:28 +0900

    DTO 수정

--- stock/src/main/java/com/zero/stock/model/Company.java
+++ stock/src/main/java/com/zero/stock/model/Company.java
@@ -2,10 +2,18 @@ package com.zero.stock.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 public class Company {
     private String ticker;
     private String name;
+
+    @Builder
+    public Company(String ticker, String name){
+        this.ticker = ticker;
+        this.name = name;
+    }
 }
--- stock/src/main/java/com/zero/stock/model/Dividend.java
+++ stock/src/main/java/com/zero/stock/model/Dividend.java
@@ -2,12 +2,20 @@ package com.zero.stock.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
 public class Dividend {
     private String dividend;
     private LocalDateTime date;
+
+    @Builder
+    public Dividend(String dividend, LocalDateTime date){
+        this.dividend = dividend;
+        this.date = date;
+    }
 }
