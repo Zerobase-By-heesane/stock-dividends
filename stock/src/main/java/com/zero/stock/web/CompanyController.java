8a7c54c6b709c4cd0b9fd14ce88290bfd1a907fa
heesane <tees3359@gmail.com>
2024-06-28 03:43:12 +0900

    addCompany

--- stock/src/main/java/com/zero/stock/web/CompanyController.java
+++ stock/src/main/java/com/zero/stock/web/CompanyController.java
@@ -1,11 +1,20 @@
 package com.zero.stock.web;
 
+import com.zero.stock.model.Company;
+import com.zero.stock.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/company")
+@Slf4j
+@RequiredArgsConstructor
 public class CompanyController {
+
+    private final CompanyService companyService;
 
     @GetMapping("/autocomplete")
     public ResponseEntity<?> autoComplete(@RequestParam String companyName) {
@@ -20,9 +29,16 @@ public class CompanyController {
     }
 
     @PostMapping("")
-    public ResponseEntity<?> addCompany(@RequestBody String companyName) {
-
-        return null;
+    public ResponseEntity<?> addCompany(@RequestBody Company request) {
+        String ticker  = request.getTicker().trim();
+        if(ObjectUtils.isEmpty(ticker)){
+            throw new RuntimeException("Ticker is empty");
+        }
+        
+        Company company = this.companyService.save(ticker);
+        
+        
+        return ResponseEntity.ok(company);
     }
 
     @DeleteMapping("")
