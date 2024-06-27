e7d18da923f228d5f6104de1b38f30c7b3d1d2ca
heesane <tees3359@gmail.com>
2024-06-28 04:14:55 +0900

    회사검색 추상메서드

--- stock/src/main/java/com/zero/stock/persist/entity/CompanyRepository.java
+++ stock/src/main/java/com/zero/stock/persist/entity/CompanyRepository.java
@@ -3,8 +3,12 @@ package com.zero.stock.persist.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<CompanyEntity,Long> {
     CompanyEntity findByTicker(String ticker);
     boolean existsByTicker(String ticker);
+
+    Optional<CompanyEntity> findByName(String name);
 }
