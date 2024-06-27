2c599b3f818c7a0481d4014c52292a07c0ac5d49
heesane <tees3359@gmail.com>
2024-06-27 23:59:28 +0900

    Month

---
+++ stock/src/main/java/com/zero/stock/model/constant/Month.java
@@ -0,0 +1,36 @@
+package com.zero.stock.model.constant;
+
+public enum Month {
+
+    JAN("Jan",1),
+    FEB("Feb",2),
+    MAR("Mar",3),
+    APR("Apr",4),
+    MAY("May",5),
+    JUN("Jun",6),
+    JUL("Jul",7),
+    AUG("Aug",8),
+    SEP("Sep",9),
+    OCT("Oct",10),
+    NOV("Nov",11),
+    DEC("Dec",12);
+
+
+
+    private String s;
+    private int number;
+
+    Month(String s, int n){
+        this.s = s;
+        this.number = n;
+    }
+
+    public static int getNumber(String s){
+        for(Month m : Month.values()){
+            if(m.s.equals(s)){
+                return m.number;
+            }
+        }
+        return -1;
+    }
+}
