05d511f01f647d3d7d472ae2e677dd397419835d
heesane <tees3359@gmail.com>
2024-06-28 18:47:11 +0900

    Like문을 통한 Auto Complete

--- stock/src/main/java/com/zero/stock/persist/entity/CompanyRepository.java
+++ stock/src/main/java/com/zero/stock/persist/entity/CompanyRepository.java
@@ -1,8 +1,11 @@
 package com.zero.stock.persist.entity;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,6 @@ public interface CompanyRepository extends JpaRepository<CompanyEntity,Long> {
     boolean existsByTicker(String ticker);
 
     Optional<CompanyEntity> findByName(String name);
+
+    Page<CompanyEntity> findByNameStartingWithIgnoreCase(String name, Pageable p);
 }
