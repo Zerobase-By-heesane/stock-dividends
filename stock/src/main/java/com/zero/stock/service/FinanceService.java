b75fd4e1dfd4e9b20a6b714e09d775da7b6a17e6
heesane <tees3359@gmail.com>
2024-06-30 04:16:02 +0900

    코드 스타일 수정

--- stock/src/main/java/com/zero/stock/service/FinanceService.java
+++ stock/src/main/java/com/zero/stock/service/FinanceService.java
@@ -6,7 +6,6 @@ import com.zero.stock.model.ScrapedResult;
 import com.zero.stock.model.constant.CacheKey;
 import com.zero.stock.persist.entity.CompanyEntity;
 import com.zero.stock.persist.entity.CompanyRepository;
-import com.zero.stock.persist.entity.DividendEntity;
 import com.zero.stock.persist.entity.DividendRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
@@ -24,20 +23,20 @@ public class FinanceService {
 
     // 요청이 자주 들어오는가? -> 자주 들어오는 경우 캐싱을 고려해볼 수 있음
     // 자주 변경되는 데이터인가? -> 자주 변경되는 데이터인 경우 캐싱을 사용하면 데이터 불일치 문제가 발생할 수 있음
-    @Cacheable(key = "#companyName", value= CacheKey.KEY_FINANCE)
-    public ScrapedResult getDividendsByCompanyName(String companyName){
+    @Cacheable(key = "#companyName", value = CacheKey.KEY_FINANCE)
+    public ScrapedResult getDividendsByCompanyName(String companyName) {
         // 1. 회사명을 기준으로 회사 정보를 조회
         CompanyEntity companyEntity = this.companyRepository.findByName(companyName).orElseThrow(
-                () -> new RuntimeException("Not found company -> "+ companyName)
+                () -> new RuntimeException("Not found company -> " + companyName)
         );
 
         // 2. 조회한 회사 정보를 기반으로 배당금 정보를 조회
         List<Dividend> dividendEntities = this.dividendRepository.findByCompanyId(companyEntity.getId())
                 .stream()
                 .map(dividendEntity -> Dividend.builder()
-                    .dividend(dividendEntity.getDividend())
-                    .date(dividendEntity.getDate())
-                    .build())
+                        .dividend(dividendEntity.getDividend())
+                        .date(dividendEntity.getDate())
+                        .build())
                 .collect(Collectors.toList());
 
         // 3. 조회한 배당금 정보를 ScrapedResult 로 변환하여 반환
