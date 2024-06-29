e7c85ac890539bd505d3c62edadfeb992929978d
heesane <tees3359@gmail.com>
2024-06-30 01:27:38 +0900

    Enable Scheduling

--- stock/src/main/java/com/zero/stock/StockApplication.java
+++ stock/src/main/java/com/zero/stock/StockApplication.java
@@ -2,8 +2,10 @@ package com.zero.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockApplication {
 
 	public static void main(String[] args) {
