51a2c9fc0a94284138f731db56eaab33dc564c7e
heesane <tees3359@gmail.com>
2024-06-30 03:58:39 +0900

    Auth Controller

--- stock/src/main/java/com/zero/stock/web/AuthController.java
+++ stock/src/main/java/com/zero/stock/web/AuthController.java
@@ -1,4 +1,34 @@
 package com.zero.stock.web;
 
+import com.zero.stock.model.Auth;
+import com.zero.stock.persist.entity.MemberEntity;
+import com.zero.stock.persist.entity.MemberRepository;
+import com.zero.stock.security.TokenProvider;
+import com.zero.stock.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final MemberService memberService;
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignUp signupRequest) {
+        return ResponseEntity.ok(this.memberService.register(signupRequest));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signIn(@RequestBody Auth.SignIn signInRequest) {
+        MemberEntity memberEntity = this.memberService.authenticate(signInRequest);
+        return ResponseEntity.ok(this.tokenProvider.generateToken(memberEntity.getUsername(), memberEntity.getRoles()));
+    }
+
 }
