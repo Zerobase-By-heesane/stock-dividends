0b3ee17508d6fe50887dd274939ba6041032ad01
heesane <tees3359@gmail.com>
2024-06-30 04:15:37 +0900

    배당금 삭제 DividendRepository

--- stock/src/main/java/com/zero/stock/persist/entity/DividendRepository.java
+++ stock/src/main/java/com/zero/stock/persist/entity/DividendRepository.java
@@ -2,6 +2,7 @@ package com.zero.stock.persist.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface DividendRepository extends JpaRepository<DividendEntity,Long> {
     List<DividendEntity> findByCompanyId(Long companyId);
     boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
+
+    @Transactional
+    void deleteAllByCompanyId(Long companyId);
 }
