da0467c5e81e45bea2eb73a226fdb2cf06a87851
heesane <tees3359@gmail.com>
2024-06-30 02:25:27 +0900

    Caching 활성화

--- stock/src/main/java/com/zero/stock/StockApplication.java
+++ stock/src/main/java/com/zero/stock/StockApplication.java
@@ -2,10 +2,12 @@ package com.zero.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class StockApplication {
 
 	public static void main(String[] args) {
