ab91f8f3c095db30ad6c0ed92120d720eb29c1d1
heesane <tees3359@gmail.com>
2024-06-30 01:26:43 +0900

    복합 유니크 키로 데이터 조회

--- stock/src/main/java/com/zero/stock/persist/entity/DividendRepository.java
+++ stock/src/main/java/com/zero/stock/persist/entity/DividendRepository.java
@@ -3,9 +3,11 @@ package com.zero.stock.persist.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DividendRepository extends JpaRepository<DividendEntity,Long> {
     List<DividendEntity> findByCompanyId(Long companyId);
+    boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
 }
