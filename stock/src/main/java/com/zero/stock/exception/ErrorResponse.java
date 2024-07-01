c8f4f011423dca149240f0899b75aedb581f241a
heesane <tees3359@gmail.com>
2024-07-01 23:30:50 +0900

    ErrorResponse 추가

---
+++ stock/src/main/java/com/zero/stock/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.zero.stock.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ErrorResponse {
+    private int code;
+    private String message;
+
+}
